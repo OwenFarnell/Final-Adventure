78bffa2c15ed238c6cb43a69ad88a5bbe7e70db6
Owen Farnell <=>
2023-02-09 11:05:15 -0700

    t3ch c0d3 d3l4y

---
+++ Final Adventure/src/Delay.java
@@ -0,0 +1,36 @@
+
+public class Delay
+	{
+		
+		public static void delay1()
+			{
+				try
+					{
+						Thread.sleep(750);
+					} catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+			}
+		public static void delay2()
+			{
+				try
+					{
+						Thread.sleep(2000);
+					} catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+			}
+		public static void delay3()
+			{
+				try
+					{
+						Thread.sleep(5000);
+					} catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+			}
+		
+	}
--- Final Adventure/src/Main.java
+++ Final Adventure/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 
 public class Main
 	{
+		static String name;
+		
+		public static void main(String [] args)
+		{
+			intro();
+		}
+		
+		public static void intro()
+			{
+				
+				Scanner userStringInput = new Scanner(System.in);
+				Scanner userIntInput = new Scanner(System.in);
 
+				System.out.println("Welcome!");
+				Delay.delay1();
+				System.out.println("When you are ready, enter your name:");
+				name = userStringInput.nextLine();
+				System.out.println("Hey " + name + ". Welcome.");
+				Delay.delay1();
+			}
+		
+		
+		
 	}
